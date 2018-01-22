2dae056e21e705c1195d56fa246583a6454d2039
Jonathan Vargas <jonathanv3232@gmail.com>
2018-01-22 00:31:21 -0500

    retrieves html from copart search

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const fs = require('fs');
+const request = require('request');
 const cheerio = require('cheerio');
 const app = express();
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+  url = 'https://www.copart.com/lotSearchResults/?free=true&query=chevy%20tahoe&page=1';
+  request(url, (err, res, html) => {
+    if (!err) {
+      const $ = cheerio.load(html);
+      let price, year, model;
+      let json = {
+        price: '',
+        year: '',
+        model: '',
+      };
+      // $('#serverSideDataTable').filter(() => {
+      //   const data = $(this);
 
-app.listen('8080');
+      //   model = data.children();
+      //   console.log(model);
+      //   console.log(data);
+      // });
+      //need to spoof user agent
+      console.log(html);
+    }
+  });
+});
+
+app.listen('8081');
 
 console.log('Ready to scrape the web');
 
